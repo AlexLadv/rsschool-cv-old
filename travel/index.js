bcb6e961c0cfcf79f465f83a7de54b26a98f4d7d
Alex <cuba-1@mail.ru>
2022-06-22 12:33:12 +0500

    feat: add basic page layout and style

---
+++ travel/index.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+ 
+  slidesPerView: 'auto',
+  spaceBetween: 60,
+  loop: true,
+  centeredSlides: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
